34f8ffb681b058e36803992e02c9bdc077498ab8
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2024-05-31 10:09:04 -0700

    gcc*: move common values to gcc_build PG
    move responsibility for maintainers, homepage,
    categories, license, master_sites, and distname to
    gcc_build PG

--- _resources/port1.0/group/gcc_build-1.0.tcl
+++ _resources/port1.0/group/gcc_build-1.0.tcl
@@ -17,6 +17,29 @@ PortGroup   select                      1.0
 # tweak MacPorts defaults
 ####################################################################################################################################
 
+default maintainers     {nomaintainer}
+
+default homepage        {https://gcc.gnu.org/}
+default categories      {lang}
+
+# an exception in the license allows dependents to not be GPL
+default license         {{GPL-3+ Permissive}}
+
+# primary releases
+default master_sites    {https://ftpmirror.gnu.org/gcc/gcc-${version}/${gcc.tag} \
+                         https://mirror.its.dal.ca/gnu/gcc/gcc-${version}/${gcc.tag} \
+                         https://mirrors.kernel.org/gnu/gcc/gcc-${version}/${gcc.tag} \
+                         https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gcc/gcc-${version}/${gcc.tag} \
+                         https://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.gnu.org/gcc/gcc-${version}/${gcc.tag} \
+                         https://mirror.yongbok.net/gnu/gcc/gcc-${version}/${gcc.tag} \
+                         http://mirror.koddos.net/gcc/releases/gcc-${version}/${gcc.tag} \
+                         ftp://ftp.gwdg.de/pub/linux/gcc/releases/gcc-${version}/${gcc.tag} \
+                         ftp://gcc.ftp.nluug.nl/mirror/languages/gcc/releases/gcc-${version}/${gcc.tag} \
+                         ftp://gcc.gnu.org/pub/gcc/releases/gcc-${version}/${gcc.tag} \
+                         gnu:gcc/gcc-${version}${gcc.tag}}
+
+default distname        {gcc-${version}}
+
 default livecheck.type  {regex}
 default livecheck.url   {http://mirror.koddos.net/gcc/releases/}
 default livecheck.regex {gcc-([option gcc.major].\[0-9.\]+)/}
@@ -24,6 +47,10 @@ default livecheck.regex {gcc-([option gcc.major].\[0-9.\]+)/}
 ####################################################################################################################################
 # define GCC options
 ####################################################################################################################################
+
+# tag for `master_sites` (see https://guide.macports.org/chunked/reference.phases.html)
+options gcc.tag
+default gcc.tag {}
 
 # stripped version of `${version}` corresponding to MacPorts ports (e.g. 4.8, 4.9, 5, 6, etc.)
 # GCC numbering scheme changes starting with version 5
@@ -41,5 +68,12 @@ default gcc.suffix  {[expr {${name} eq "gcc-devel" ? "devel" : ${gcc.major}}]}
 namespace eval gcc_build {}
 
 proc gcc_build::callback {} {
+    global prefix name subport version
+
+    if { [vercmp ${version} >= 5] } {
+        use_xz      yes
+    } else {
+        use_bzip2   yes
+    }
 }
 port::register_callback gcc_build::callback
