d77f276c8170a2e24e53963eeaab901445ae27ff
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2024-05-31 09:21:44 -0700

    gcc*: move responsibility for major version to PG

--- _resources/port1.0/group/gcc_build-1.0.tcl
+++ _resources/port1.0/group/gcc_build-1.0.tcl
@@ -14,6 +14,20 @@ PortGroup   gcc_info                    1.0
 PortGroup   select                      1.0
 
 ####################################################################################################################################
+# define GCC options
+####################################################################################################################################
+
+# stripped version of `${version}` corresponding to MacPorts ports (e.g. 4.8, 4.9, 5, 6, etc.)
+# GCC numbering scheme changes starting with version 5
+#     see https://gcc.gnu.org/develop.html#num_scheme
+options gcc.major
+default gcc.major   {[expr {[vercmp ${version} >= 5] ? [lindex [split ${version} .-] 0] : [join [lrange [split ${version} .-] 0 1] .]}]}
+
+# suffix to add to files to void conflict with various GCC ports
+options gcc.suffix
+default gcc.suffix  {[expr {${name} eq "gcc-devel" ? "devel" : ${gcc.major}}]}
+
+####################################################################################################################################
 # internal procedures
 ####################################################################################################################################
 namespace eval gcc_build {}
